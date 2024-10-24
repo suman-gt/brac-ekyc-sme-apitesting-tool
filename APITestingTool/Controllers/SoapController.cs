4ab20f85d03e9a951b3909773afb402eba6af10c
suman <suman.ahmed@gigatechltd.com>
2024-10-24 17:40:13 +0600

    Initial Commit

---
+++ APITestingTool/Controllers/SoapController.cs
@@ -0,0 +1,47 @@
+﻿using APITestingTool.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace APITestingTool.Controllers
+{
+    public class SoapController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public SoapController()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public ActionResult Index()
+        {
+            return View(new SoapRequestModel());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Index(SoapRequestModel model)
+        {
+            try
+            {
+                string response = await CallSoapApiAsync(model.SoapAction, model.SoapBody, model.SoapNamespace);
+                model.Response = response;
+            }
+            catch (Exception ex)
+            {
+                model.Response = $"Error calling SOAP API: {ex.Message}";
+            }
+
+            return View(model);
+        }
+
+        private async Task<string> CallSoapApiAsync(string soapAction, string soapBody,string soapNamespace)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, soapNamespace);
+            request.Headers.Add("SOAPAction", soapAction);
+            request.Content = new StringContent(soapBody, Encoding.UTF8, "text/xml");
+
+            var response = await _httpClient.SendAsync(request);
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ APITestingTool/Models/SoapRequestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace APITestingTool.Models
+{
+    public class SoapRequestModel
+    {
+
+        public string  SoapAction { get; set; }
+        public string SoapNamespace { get; set; }
+        public string SoapBody { get; set; }
+        public string Response { get; set; }
+    }
+}
